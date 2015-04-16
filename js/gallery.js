d2834262719a5d7183149cf53da79662a41bb6fd
Siavash Rezaei <siavash@Siavashs-MacBook-Pro.local>
2015-04-16 10:22:46 -0700

    First commit, website almost transffered to Jekyll

---
+++ js/gallery.js
@@ -0,0 +1,189 @@
+/*
+This code manages the gallery block
+*/
+/*global $, jQuery*/
+$(document).ready(function() {
+    var gallery = {};
+    gallery.isZoomed = false;
+
+    $('.gallery > div').click(function(){
+        var windowHeight = $(window).height(),
+            windowWidth = $(window).width();
+        if(gallery.isZoomed == true){
+            //Zoom out
+            var imageTile = $(this);
+            var tl = new TimelineLite();
+            tl.to($('#zoomedImageContainer'), 0.3, {
+                autoAlpha : 0,
+                ease: Power2.easeOut,
+                overwrite: 5
+            }).to($('.gallery'), 0.5, {
+                css: {force3D:true, transform:'translateX(0px) translateY(0px) scale(1)' },
+                ease: Power2.easeInOut,
+                onComplete: function(){
+                    gallery.isZoomed = false;
+                    $('#hiresImage').remove();
+                }
+            }).to($(this).children('.imageTile'), 0.3, {
+                autoAlpha : 1,
+                ease: Power2.easeOut,
+                overwrite: 5
+            });
+        } else{
+            //Zoom in
+            //Storing the zoom state. doesn't make sense to store this in this element, but it does the job
+            //$(this).data('zoomData' , {isZoomed : true});
+            gallery.isZoomed = true;
+            //calculating the scale and translate for the image we are going to zoomed on
+            var transformMatrix = matrixToArray($(this).css('transform'));
+            var tileWidth = $(this).width();
+            var tileHeight = $(this).height();
+            var scaleFactor = (windowWidth/tileWidth);
+            //scaleFactor = Math.max( (windowWidth/tileWidth), (windowHeight/tileHeight) );
+            var x_translate = ((windowWidth/2) - transformMatrix[4] - (tileWidth/2))*scaleFactor;
+            var y_translate = windowHeight/2 - 2*transformMatrix[5];
+            //Get hi-res image url
+            var hires_src = $(this).attr('data-zoomImg');
+            var hires_bgSize = $(this).attr('data-bgSize');
+            var tl = new TimelineLite();
+            tl.to($(this).children('.imageTile'), 0.3, {
+                autoAlpha : 0,
+                ease: Power2.easeOut,
+                overwrite: 5
+            }).to($('.gallery'), 0.5, {
+                css: {force3D:true, transform:'translateX('+ x_translate +'px) translateY('+y_translate+'px) scale('+scaleFactor+')' },
+                ease: Power2.easeInOut,
+            }).to($('#zoomedImageContainer'), 0.3, {
+                autoAlpha : 1,
+                ease: Power2.easeOut,
+                overwrite: 5,
+                onComplete: function(){
+                    spinner.spin(spinnerTarget);
+                    loadingIcon.animateIn();
+                    var img = $("<img />").attr({'src': hires_src, 'id' : 'hiresImage'}).css('opacity', 0)
+                    .load(function() {
+                        if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
+                            alert('broken image!');
+                        } else {
+                            //$("#zoomedImageContainer").append(img);
+                            $("#zoomedImageContainer").prepend($("<div></div>").attr({'id' : 'hiresImage'}).css({'opacity': 0, 'background-image':'url('+hires_src+')', 'background-size': hires_bgSize}).addClass('imageTile'));
+                            var imgLoad_tl = new TimelineLite();
+                            imgLoad_tl.add(loadingIcon.animateOut()).to($('#hiresImage'), 0.5, {autoAlpha:1});
+                        }
+                    });
+                }
+            });
+
+        }
+
+    });
+
+
+
+    function calcGalleryLayout(){
+        /*This function calculates size and position of gallery and it's tiles*/
+        var winHeight = $(window).height();
+        var winWidth = $(window).width();
+        $('.gallery_container').height(winHeight);
+        $('.gallery_container').width(winWidth);
+        $('.image1_container').width(winWidth/5);
+        $('.image1_container').height(winHeight);
+        $('.image1_container').css('transform', 'translate3d(0,0,0)');
+        $('.image2_container').width(winWidth/5);
+        $('.image2_container').height(winHeight/2);
+        $('.image2_container').css('transform', 'translate3d('+winWidth/5 +'px,0,0)');
+        $('.image3_container').width(winWidth/2.5);
+        $('.image3_container').height(winHeight/2);
+        $('.image3_container').css('transform', 'translate3d('+2*winWidth/5+'px,0,0)');
+        $('.image4_container').width(winWidth/5);
+        $('.image4_container').height(winHeight/2);
+        $('.image4_container').css('transform', 'translate3d('+4*winWidth/5+'px,0,0)');
+        $('.image5_container').width(winWidth/2.5);
+        $('.image5_container').height(winHeight/2);
+        $('.image5_container').css('transform', 'translate3d('+winWidth/5+'px,'+winHeight/2+'px,0)');
+        $('.image6_container').width(winWidth/2.5);
+        $('.image6_container').height(winHeight/2);
+        $('.image6_container').css('transform', 'translate3d('+2*winWidth/3.33+'px,'+winHeight/2+'px,0)');
+        $('.image7_container').width(winWidth/2.5);
+        $('.image7_container').height(winHeight/2);
+        $('.image7_container').css('transform', 'translate3d('+3*winWidth/5+'px,'+winHeight/2+'px,0)');
+    }
+
+    calcGalleryLayout();
+    $( window ).resize(function() {
+        calcGalleryLayout();
+    });
+
+    $('.imageTile').hover(function(event){
+        //hoverIn for image tiles in gallery
+        TweenLite.to( $(this), 0.3,
+                     {css:{scale:1.1},
+                      ease:Power2.easeOut
+                     });
+    }, function(){
+        //hoverOut for image tiles in gallery
+        TweenLite.to($(this), 0.3,
+                     {css:{scale:1},
+                      ease:Power2.easeOut
+                     });
+    });
+
+
+    var opts = {
+        lines: 15, // The number of lines to draw
+        length: 24, // The length of each line
+        width: 2, // The line thickness
+        radius: 60, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 36, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#9d6530', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 40, // Afterglow percentage
+        //shadow: true, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '50%', // Top position relative to parent
+        left: '50%' // Left position relative to parent
+    };
+    var spinnerTarget = document.getElementById('loader_wrapper');
+    var spinner = new Spinner(opts);
+
+    var loadingIcon = {
+        fadeTime : 0.4,
+        animateIn : function(){
+            spinner.spin(spinnerTarget);
+            var tl = new TimelineLite();
+            tl.to($('.spinner'), this.fadeTime, {
+                autoAlpha:1,
+            } ).to($(' #gallery_emblem'), this.fadeTime, {
+                autoAlpha: 0.2,
+            }, 0);
+            return tl;
+        },
+        animateOut : function(){
+            var tl = new TimelineLite();
+            tl.to($('.spinner, #gallery_emblem'), this.fadeTime, {
+                autoAlpha:0,
+                onComplete: function(){
+                    spinner.stop();
+                }
+            });
+            return tl;
+        }
+    }
+
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) {
+            // esc
+            if(gallery.isZoomed == true)  $('.gallery > div').click();    //triggers zoom out
+        }
+    });
+
+
+    function matrixToArray(str){
+        /*Get a string matrix and returns an array*/
+        return str.match(/(-?[0-9\.]+)/g);
+    };
+});
---
+++ js/sereneaudio.js
@@ -0,0 +1,301 @@
+//we'll use a window.onload for simplicity, but typically it is best to use either jQuery's $(document).ready() or $(window).load() or cross-browser event listeners so that you're not limited to one.
+
+
+
+$( document ).ready(function() {
+    var body = document.body,
+        timer;
+    var winHeight = $(window).height();
+    $('.section').height(winHeight);
+
+    /*Dissable hover for better scrolling performance*/
+    window.addEventListener('scroll', function() {
+        clearTimeout(timer);
+        if(!body.classList.contains('disable-hover')) {
+            body.classList.add('disable-hover')
+        }
+
+        timer = setTimeout(function(){
+            body.classList.remove('disable-hover')
+        },500);
+    }, false);
+
+    //var ScrollMagic_ctrl = new ScrollMagic();
+    /*
+    ScrollMagic_ctrl.scrollTo(function (newpos) {
+        TweenMax.to(window, 1, {
+            scrollTo: {y: newpos, autoKill:true},
+            ease: Power2.easeInOut
+        });
+    });
+
+
+    var wiring_tl = new TimelineMax();
+    wiring_tl.fromTo($('#leftWire'),1, {css: {bottom: "70px"}}, {css:{bottom:0}} ).
+    fromTo($('#horizWire'),2, {width:"0%"}, {width:"100%"}).
+    fromTo($('#rightWire'),1, {css: {top:"70px"}}, {css:{top:"-5px"}}).
+    fromTo($('#sound_pulse'),1, {css:{scale:0.4, opacity:0}},{css:{scale:1.9, opacity: 1}, ease: Power2.easeIn},'-=0.5').
+    to($('#sound_pulse'),6, {css:{scale:3.4, opacity:0}, ease: Power2.easeOut});
+
+    var applcation_scene = new ScrollScene({triggerElement: "#application_trigger", duration: winHeight/2})
+    .setPin("#application")
+    //.setTween(wiring_tl)
+    .addTo(ScrollMagic_ctrl)
+    .triggerHook(0);
+    //.addIndicators({zindex: 100, colorEnd: "#FFFFFF"});
+
+    var gallery_scene = new ScrollScene({triggerElement: "#gallery_trigger", duration: winHeight/2})
+    .setPin("#gallery")
+    .addTo(ScrollMagic_ctrl)
+    .triggerHook(0);
+    //.addIndicators({zindex: 100, colorEnd: "#FFFFFF"});
+    */
+
+
+
+    var $window = $(window);
+    var scroll_tween = TweenMax.to($window, 1, {
+        scrollTo : { y: 0, autoKill:true },
+        ease: Power2.easeOut,
+
+    });
+    scroll_tween.pause();
+
+    $(document).on("click", "a[href^=#]", function (e) {
+        var id = $(this).attr("href");
+        if ($(id).length > 0) {
+            e.preventDefault();
+            scroll_tween.updateTo({scrollTo : { y: $(id).offset().top }} , true);
+            scroll_tween.restart();
+            // if supported by the browser we can even update the URL.
+            if (window.history && window.history.pushState) {
+                history.pushState("", document.title, id);
+            }
+        }
+    });
+
+    var scrollTop, finalScroll, lastUpdateTime = 0;
+    $window.on("mousewheel DOMMouseScroll", function(event){
+        //scrolls the window one block
+        event.preventDefault();
+        //return;
+        var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+
+        if(!scroll_tween.isActive() ){
+            if ((Math.abs(delta) < 0.1) ) {return false};
+            //console.log("time since last animation: ", $.now() -  lastUpdateTime);
+            scrollTop = $window.scrollTop();
+            finalScroll = scrollTop + (delta<0 ? winHeight : -winHeight);
+            scroll_tween.updateTo({scrollTo : { y: finalScroll }} , true);
+            scroll_tween.restart();
+            lastUpdateTime = $.now();
+        }else if ((Math.abs(delta) >= 2.5) && ($.now() -  lastUpdateTime > 600)){
+            //if if the user scrolled again while the tween is playing, we update the scroll
+            finalScroll = finalScroll + (delta<0 ? winHeight : -winHeight);
+            scroll_tween.updateTo({scrollTo : { y: finalScroll }} , true);
+            lastUpdateTime = $.now();
+        }
+        return false;
+    });
+
+
+
+    $('html').keydown(function(e){
+        //Arrow keys create a fake mousewheel event.
+        //This way, I can use the same fucntion that I use for handling mousewheel events for arrow keys as well
+        event.preventDefault();
+        var fakeEvent = jQuery.Event( "mousewheel" );
+        fakeEvent.originalEvent = {};
+        if(e.which == 38){
+            //up
+            fakeEvent.originalEvent.wheelDelta = 300;
+            $window.trigger(fakeEvent);
+        }else if(e.which == 40){
+            //down
+            fakeEvent.originalEvent.wheelDelta = -300;
+            $window.trigger(fakeEvent);
+        }
+    });
+
+
+    var scrollTimer;
+    $window.scroll(function(){
+        /*When user manually drags the scroll bar on the window, we want to adjust the scroll position so that a whole block is visible.
+            This won't allow for scroll position to be in between blocks*/
+        clearTimeout(scrollTimer);
+        scrollTimer=setTimeout(function(){
+            adjustScroll();
+        }, 500);
+    });
+
+    var wiring_tl = new TimelineLite();
+    wiring_tl.fromTo($('#leftWire'),0.6, {height: 0}, {height: 80} ).
+    fromTo($('#horizWire'),1, {width:"0%"}, {width:"100%"}).
+    fromTo($('#rightWire'),0.6, {height: 0}, {height: 80}).
+    fromTo($('#sound_pulse'),1, {css:{scale:0.4, opacity:0}},{css:{scale:1.9, opacity: 1}, ease: Power2.easeIn},'-=0.5').
+    to($('#sound_pulse'),1, {css:{scale:3.4, opacity:0}, ease: Power2.easeOut});
+    wiring_tl.pause();
+    $('#wiring').appear(function(){
+        wiring_tl.restart();
+    }, {one: false});
+
+
+    $('.appear_mark').appear(function(){
+        var appeared_block_id = $(this).parent().attr('id');
+        //console.log($(this).parent().attr('id'));
+        $('#scrollNav > ul > li > a').each(function(){
+            var href = $(this).attr('href');
+            if(href.indexOf(appeared_block_id) >= 0){
+                $(this).addClass('current');
+            }else{
+                $(this).removeClass('current');
+            }
+        });
+
+        if(appeared_block_id == 'sound'){
+            /*Runs the animation that lets the user know the content is hoverable*/
+            $('.isotopeSummary').removeClass('enableHover');
+            $('.animated_bg_img').removeClass('animated_bg_img_hover');
+            //$('.animated_bg_img').off( "mouseenter" );
+
+            var sound_tl = new TimelineLite({onComplete:function(){
+                $('.isotopeSummary').addClass('enableHover');
+                $('.animated_bg_img').addClass('animated_bg_img_hover');
+
+                $('.animated_bg_img_hover').hover(function(event){
+                    console.log( $(this).children(".sharp_img").attr('class'));
+                    //hoverIn for image tiles in gallery
+                    TweenLite.to( $(this).children(".scale_blur_img_container"), 0.4,
+                                 {css:{force3D:true, top: 0 },
+                                  ease:Power2.easeOut
+                                 });
+                    TweenLite.to( $(this).children(".scale_blur_img_container").children(".sharp_img"), 0.4,
+                                 {css:{force3D:true, force3D:true, scale:0.4, opacity: 0 },
+                                  ease:Power2.easeOut
+                                 });
+                    TweenLite.to( $(this).children(".scale_blur_img_container").children(".blur_img"), 0.4,
+                                 {css:{force3D:true, scale:0.4, opacity: 0.8},
+                                  ease:Power2.easeOut
+                                 });
+                }, function(){
+                    //hoverOut for image tiles in gallery
+                    TweenLite.to( $(this).children(".scale_blur_img_container"), 0.4,
+                                 {css:{force3D:true, top: "15%" },
+                                  ease:Power2.easeOut
+                                 });
+                    TweenLite.to( $(this).children(".scale_blur_img_container").children(".sharp_img"), 0.4,
+                                 {css:{force3D:true, force3D:true, scale:1, opacity: 1},
+                                  ease:Power2.easeOut
+                                 });
+                    TweenLite.to( $(this).children(".scale_blur_img_container").children(".blur_img"), 0.4,
+                                 {css:{force3D:true, scale:1, opacity: 0},
+                                  ease:Power2.easeOut
+                                 });
+                });
+
+                /*
+                $('.animated_bg_img_hover').hover(function(event){
+                    //hoverIn for image tiles in gallery
+                    TweenLite.to( $(this), 0.3,
+                                 {css:{backgroundSize:'40% 40%' },
+                                  ease:Power2.easeOut
+                                 });
+                }, function(){
+                    //hoverOut for image tiles in gallery
+                    TweenLite.to($(this), 0.3,
+                                 {css:{backgroundSize:'70% 70%' },
+                                  ease:Power2.easeOut
+                                 });
+                });
+                */
+            }, delay:0.5});
+
+
+            sound_tl
+            //Driver
+            .to($('#driver'), 0.3,
+                { css: {force3D:true, transform:'translateX(0px) translateY(-90px) scale(1)' }, ease: Linear.ease})
+            .to($('#driver'), 0.3,
+                { css: {force3D:true, transform:'translateX(0px) translateY(0px) scale(1)' }, ease: Linear.ease})
+            .fromTo($('#driver > .isotopeDetailed'), 0.2,
+                    { css: {opacity:1}},
+                    { css: {opacity:0}, ease: Expo.easeOut, onComplete: function(){
+                        $('#driver > .isotopeDetailed').css('opacity','');
+                    }},
+                    0.3)
+            //Eclosure
+            .to($('#enclosure'), 0.3,
+                { css: {force3D:true, transform:'translateX(0px) translateY(-90px) scale(1)' }, ease: Linear.ease}, 0.1)
+
+            .to($('#enclosure'), 0.3,
+                { css: {force3D:true, transform:'translateX(0px) translateY(0px) scale(1)' }, ease: Linear.ease}, 0.4)
+
+            .fromTo($('#enclosure > .isotopeDetailed'), 0.2,
+                    { css: {opacity:1}},
+                    { css: {opacity:0}, ease: Linear.ease, onComplete: function(){
+                        $('#enclosure > .isotopeDetailed').css('opacity','');
+                    }},
+                    0.4)
+            .to($('#amplifier'), 0.3,
+                { css: {force3D:true, transform:'translateX(0px) translateY(-90px) scale(1)' }, ease: Linear.ease}, 0.2)
+
+            .to($('#amplifier'), 0.3,
+                { css: {force3D:true, transform:'translateX(0px) translateY(0px) scale(1)' }, ease: Linear.ease}, 0.5)
+
+            .fromTo($('#amplifier > .isotopeDetailed'), 0.2,
+                    { css: {opacity:1}},
+                    { css: {opacity:0}, ease: Linear.ease, onComplete: function(){
+                        $('#amplifier > .isotopeDetailed').css('opacity','');
+                    }},
+                    0.5)
+            .timeScale( 1 );
+        }
+
+    }, {one: false, accX: 0, accY: -2});
+
+
+
+
+
+    function RunBlockAnimation(scrollToBlockId){
+        if(scrollToBlockId == 'application'){
+
+        }
+    }
+
+    $( window ).resize(function() {
+        /*Adjust block heights and scroll position on window resize*/
+        winHeight = $(window).height();
+        $('.section').height(winHeight);
+        adjustScroll();
+        //applcation_scene.duration(winHeight/2);
+        //gallery_scene.duration(winHeight/2);
+    });
+
+    function    adjustScroll(){
+        /*Adjust scroll position when a block is not perfectly aligned with the viewport*/
+        $('.block_wrapper').each(function(){
+            //console.log($(this).offset().top);
+            var viewportTop = $(this).offset().top - $(window).scrollTop()
+            if((viewportTop > 0) && (viewportTop < winHeight/2)){
+                TweenMax.to($window, 0.5, {
+                    scrollTo : { y: $(this).offset().top, autoKill:true },
+                    ease: Power2.easeInOut,
+                    overwrite: "all",
+                    onComplete: function(){
+                    }
+                })
+            }else if((viewportTop < winHeight) && (viewportTop > winHeight/2)){
+                TweenMax.to($window, 0.5, {
+                    scrollTo : { y: $(this).offset().top - winHeight, autoKill:true },
+                    ease: Power2.easeInOut,
+                    overwrite: "all",
+                    onComplete: function(){
+                    }
+                })
+            }
+        });
+    }
+    //TweenLite.to(window, 1, { css:{top:designOffset}, overwrite:true } );
+});
+
