62d9d6ad786b2391f4cc02c49cc2baec720d301b
Siavash Rezaei <siavash@MacBook-Pro-2.local>
2016-08-08 16:54:22 -0700

    changed the order of Google Analytics calls to capture custom dimetions

--- js/sereneaudio.js
+++ js/sereneaudio.js
@@ -1,19 +1,5 @@
 //we'll use a window.onload for simplicity, but typically it is best to use either jQuery's $(document).ready() or $(window).load() or cross-browser event listeners so that you're not limited to one.
 
-ABalytics.init({
-    experiment1_name: [
-        {
-            name: 'variant1_name',
-            "experiment1_class1_name": "wireless speaker system"
-        },
-        {
-            name: 'variant2_name',
-            "experiment1_class1_name": "product"
-        }
-    ]
-
-});
-
 $( document ).ready(function() {
     ABalytics.applyHtml();
 
